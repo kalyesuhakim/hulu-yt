07bf9c8ddb92cf230d46dd0f9deaad680fe7c2f1
unknown <kalyesuhakim54@gmail.com>
2022-02-26 06:17:32 +0300

    updat2

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,4 +1,4 @@
-import Thumbnail from './components/Thumbnail'
+import Thumbnail from './Thumbnail'
 function Results({results}){
     return(
         <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap items-center">
